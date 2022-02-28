41a615a2ec6b7932a5aeaf70771ea114e30ed7ee
Andrea Miotto <miotto.andrea@icloud.com>
2022-02-28 22:52:28 +0000

    Refactor project package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PartialSheet",
-    platforms: [.iOS(.v15), .macOS(.v12)],
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,9 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PartialSheet",
-            dependencies: []),
-        .testTarget(
-            name: "PartialSheetTests",
-            dependencies: ["PartialSheet"]),
+            dependencies: [])
     ]
 )
