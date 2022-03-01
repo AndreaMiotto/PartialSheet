23091caf5995e0e8baa15b0cc4fbb82b48c0a962
Andrea Miotto <miotto.andrea@icloud.com>
2022-03-01 12:59:57 +0000

    Update package manifest

--- Package.swift
+++ Package.swift
@@ -21,6 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PartialSheet",
-            dependencies: [])
+            dependencies: [],
+            path: "Sources"
+        )
     ]
 )
